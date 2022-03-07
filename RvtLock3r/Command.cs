1e0db2adb897913b8be55eb09974155156add24d
Jeremy Tammik <jeremy.tammik@autodesk.com>
2022-03-07 08:42:58 +0100

    store validation error elements and params for better reporting

--- RvtLock3r/Command.cs
+++ RvtLock3r/Command.cs
@@ -32,11 +32,17 @@ namespace RvtLock3r
       string rvtpath = doc.PathName;
       string txtpath = rvtpath.Replace(".rvt", ".lock3r");
       string[] lines = File.ReadAllLines(txtpath);
-      List<string> log = new List<string>();
+
+      // Store validation error element and parameter ids
+
+      Dictionary<int, List<Guid>> errorLog 
+        = new Dictionary<int, List<Guid>>();
+
       foreach (string line in lines )
       {
         string[] triple = line.Split(null);
-        ElementId eid = new ElementId(int.Parse(triple[0]));
+        int i = int.Parse(triple[0]);
+        ElementId eid = new ElementId(i);
         Guid pid = new Guid(triple[1]);
         string checksum = triple[2];
         Element e = doc.GetElement(eid);
@@ -45,9 +51,17 @@ namespace RvtLock3r
         string pchecksum = ComputeChecksum(pval);
         if( !checksum.Equals(pchecksum))
         {
-          log.Add(string.Format(
-            "Validation error on element/parameter '{0}' -- '{1}'",
-            ElementDescription(e), p.Definition.Name));
+          //log.Add(string.Format(
+          //  "Validation error on element/parameter '{0}' -- '{1}'",
+          //  ElementDescription(e), p.Definition.Name));
+          if (!errorLog.ContainsKey(i))
+          {
+            errorLog.Add(i,new List<Guid>());
+          }
+          if(!errorLog[i].Contains(pid))
+          {
+            errorLog[i].Add(pid);
+          }
         }
       }
 
@@ -84,9 +98,17 @@ namespace RvtLock3r
       }
       */
 
-      if( 0 < log.Count )
+      int n = errorLog.Count;
+
+      if( 0 < n )
       {
         // Report errors to user
+        // Set reference return values ElementSet elements and message
+        
+        if( 1 == n )
+        {
+
+        }  
 
         return Result.Failed;
       }
