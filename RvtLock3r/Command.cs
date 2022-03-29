f2f028e3856c421b010554c0a2a6a1e4e46f2645
Jeremy Tammik <jeremy.tammik@autodesk.com>
2022-03-29 08:55:07 +0200

    whitespace

--- RvtLock3r/Command.cs
+++ RvtLock3r/Command.cs
@@ -17,81 +17,78 @@ using System.Text;
 
 namespace RvtLock3r
 {
-    [Transaction(TransactionMode.ReadOnly)]
-    public class Command : IExternalCommand
+  [Transaction(TransactionMode.ReadOnly)]
+  public class Command : IExternalCommand
+  {
+    public Result Execute(
+      ExternalCommandData commandData,
+      ref string message,
+      ElementSet elements)
     {
-        public Result Execute(
-          ExternalCommandData commandData,
-          ref string message,
-          ElementSet elements)
+      UIApplication uiapp = commandData.Application;
+      UIDocument uidoc = uiapp.ActiveUIDocument;
+      Application app = uiapp.Application;
+      Document doc = uidoc.Document;
+
+      string rvtpath = doc.PathName;
+      string txtpath = rvtpath.Replace(".rte", ".lock3r");
+
+      string[] lines = File.ReadAllLines(txtpath);
+
+      // In case of validation error, store element and parameter ids
+
+      Dictionary<int, List<Guid>> errorLog
+        = new Dictionary<int, List<Guid>>();
+
+      for (int j = 0; j < lines.Length - 1; j++)
+      {
+        string[] triple = lines[j].Split(null);
+        string id = triple[0];
+        int i = int.Parse(triple[0]);
+        ElementId eid = new ElementId(i);
+        Guid pid = new Guid(triple[1]);
+        string checksum = triple[2];
+
+        Element e = doc.GetElement(eid);
+
+        Parameter p = e.get_Parameter(pid);
+
+        string pval = Util.ParameterToString(p);
+
+        string pchecksum = Util.ComputeChecksum(pval);
+
+        if (!checksum.Equals(pchecksum))
         {
-            UIApplication uiapp = commandData.Application;
-            UIDocument uidoc = uiapp.ActiveUIDocument;
-            Application app = uiapp.Application;
-            Document doc = uidoc.Document;
+          //log.Add(string.Format(
+          //  "Validation error on element/parameter '{0}' -- '{1}'",
+          //  ElementDescription(e), p.Definition.Name));
 
-            string rvtpath = doc.PathName;
-            string txtpath = rvtpath.Replace(".rte", ".lock3r");
+          if (!errorLog.ContainsKey(i))
+          {
+            errorLog.Add(i, new List<Guid>());
+          }
+          if (!errorLog[i].Contains(pid))
+          {
+            errorLog[i].Add(pid);
+          }
 
-            string[] lines = File.ReadAllLines(txtpath);
-
-            // In case of validation error, store element and parameter ids
-
-            Dictionary<int, List<Guid>> errorLog
-              = new Dictionary<int, List<Guid>>();
-
-            for (int j = 0; j < lines.Length - 1; j++)
-            {
-                string[] triple = lines[j].Split(null);
-                string id = triple[0];
-                int i = int.Parse(triple[0]);
-                ElementId eid = new ElementId(i);
-                Guid pid = new Guid(triple[1]);
-                string checksum = triple[2];
-
-                Element e = doc.GetElement(eid);
-
-             
-
-                Parameter p = e.get_Parameter(pid);
-
-                string pval = Util.ParameterToString(p);
-
-                string pchecksum = Util.ComputeChecksum(pval);
-
-                if (!checksum.Equals(pchecksum))
-                {
-                    //log.Add(string.Format(
-                    //  "Validation error on element/parameter '{0}' -- '{1}'",
-                    //  ElementDescription(e), p.Definition.Name));
-
-                    if (!errorLog.ContainsKey(i))
-                    {
-                        errorLog.Add(i, new List<Guid>());
-                    }
-                    if (!errorLog[i].Contains(pid))
-                    {
-                        errorLog[i].Add(pid);
-                    }
-                    
-                }
-            }
-
-            int n = errorLog.Count;
-
-            if (0 < n)
-            {
-                // Report errors to user
-                // Set reference return values ElementSet elements and message
-                message = "Model Paramaters have been Altered!";
-
-                 Util.GetAlteredElements(doc, errorLog, elements);
-
-                return Result.Failed;
-            }
-            return Result.Succeeded;
         }
+      }
 
-      
+      int n = errorLog.Count;
+
+      if (0 < n)
+      {
+        // Report errors to user by setting the
+        // ElementSet `elements` and `message` return values
+
+        message = "Model Paramaters have been Altered!";
+
+        Util.GetAlteredElements(doc, errorLog, elements);
+
+        return Result.Failed;
+      }
+      return Result.Succeeded;
     }
+  }
 }
