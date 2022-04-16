84d58935f57d0ff0ce893b8c8402e77dbbc63071
Jeremy Tammik <jeremy.tammik@autodesk.com>
2022-04-16 10:32:35 +0200

    implemented CreateGroundTruthFor

--- RvtLock3r/CmdGroundTruth.cs
+++ RvtLock3r/CmdGroundTruth.cs
@@ -11,6 +11,32 @@ namespace RvtLock3r
   [Transaction(TransactionMode.ReadOnly)]
   public class CmdGroundTruth : IExternalCommand
   {
+    /// <summary>
+    /// Create ground truth for given document.
+    /// </summary>
+    void CreateGroundTruthFor(Document doc)
+    {
+      string rvtpath = doc.PathName;
+      string txtpath = rvtpath.Replace(".rte", ".lock3r");
+
+      FilteredElementCollector wallTypes
+        = new FilteredElementCollector(doc)
+          .OfClass(typeof(WallType));
+
+      string s = string.Empty;
+
+      foreach (Element e in wallTypes)
+      {
+        s += Util.GroundTruthData(e);
+      }
+
+      if (File.Exists(txtpath))
+      {
+        File.Delete(txtpath);
+      }
+      File.WriteAllText(txtpath, s);
+    }
+
     public Result Execute(
       ExternalCommandData commandData,
       ref string message,
@@ -47,34 +73,34 @@ namespace RvtLock3r
       return Result.Succeeded;
     }
 
-        public Result Execute(UIApplication uiapp)
-        {
-            Document doc = uiapp.ActiveUIDocument.Document;
-            string rvtpath = doc.PathName;
-            string txtpath = rvtpath.Replace(".rte", ".lock3r");
+    public Result Execute(UIApplication uiapp)
+    {
+      Document doc = uiapp.ActiveUIDocument.Document;
+      string rvtpath = doc.PathName;
+      string txtpath = rvtpath.Replace(".rte", ".lock3r");
 
-            // Retrieve elements from database
+      // Retrieve elements from database
 
-            FilteredElementCollector wallTypes
-              = new FilteredElementCollector(doc)
-                .OfClass(typeof(WallType));
+      FilteredElementCollector wallTypes
+        = new FilteredElementCollector(doc)
+          .OfClass(typeof(WallType));
 
-            string allString = string.Empty;
+      string allString = string.Empty;
 
-            foreach (Element e in wallTypes)
-            {
-                string s = Util.GroundTruthData(e);
-                allString += s;
-            }
+      foreach (Element e in wallTypes)
+      {
+        string s = Util.GroundTruthData(e);
+        allString += s;
+      }
 
-            if (File.Exists(txtpath))
-            {
-                File.Delete(txtpath);
-            }
-            File.WriteAllText(txtpath, allString);
+      if (File.Exists(txtpath))
+      {
+        File.Delete(txtpath);
+      }
+      File.WriteAllText(txtpath, allString);
 
-            return Result.Succeeded;
+      return Result.Succeeded;
 
-        }
     }
+  }
 }
